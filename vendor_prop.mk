33b5482eafeece7a88113c6c8f355d8be2b63a1c
Mansur Alisha Shaik <mansur@codeaurora.org>
2018-03-12 19:53:36 +0530

    msm8937-common: Disable UBWC mode for 8953
    Added system prop to disable UBWC mode
    on 8953 target.
    
    CRs-Fixed: 2203200
    Change-Id: I5653f010f26808d255288533195d4d22756d252d

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -106,7 +106,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.vidc.enc.disable_bframes=1 \
     vendor.vidc.enc.disable_pframes=1 \
     vendor.vidc.enc.narrow.searchrange=1 \
-    vendor.vidc.disable.split.mode=1
+    vendor.vidc.disable.split.mode=1 \
+    video.disable.ubwc=1
 
 # Memory optimizations
 PRODUCT_PROPERTY_OVERRIDES += \
