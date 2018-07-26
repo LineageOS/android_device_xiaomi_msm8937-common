84451ea5f7e414f445dc6d274cff201e76e43268
Lakshman Chaluvaraju <lchalu@codeaurora.org>
2018-07-26 15:04:37 +0530

    msm8937-common: Move Audio HAL from 2.0 to 4.0
    - Moving Audio HAL from 2.0 to 4.0.
    
    Change-Id: I623d92bae679a89b0af4863d315f34edaf30019f

--- msm8937.mk
+++ msm8937.mk
@@ -63,9 +63,9 @@ PRODUCT_PACKAGES += \
     libqcomvoiceprocessing
 
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
+    android.hardware.audio@4.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.effect@4.0-impl \
     android.hardware.soundtrigger@2.1-impl
 
 PRODUCT_COPY_FILES += \
