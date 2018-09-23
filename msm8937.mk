6eeb76a6dbf9a19fc9aa8c3a56c7b64e7a34fd64
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-09-23 12:44:50 +0200

    msm8937-common: Build libsensorndkbridge for gnss
    Change-Id: I3327a75f6d7ee910f04f574710fad54aed92e120

--- msm8937.mk
+++ msm8937.mk
@@ -147,7 +147,8 @@ PRODUCT_PACKAGES += \
 # GPS
 PRODUCT_PACKAGES += \
     libgnss \
-    libgnsspps
+    libgnsspps \
+    libsensorndkbridge
 
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.1-impl-qti \
