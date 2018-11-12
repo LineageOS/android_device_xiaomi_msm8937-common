15539a11fbe30fe18f1862f8da3a04c07d8b4ce3
Bruno Martins <bgcngm@gmail.com>
2018-11-12 16:36:38 +0000

    msm8937-common: Update GPS HAL to P
     * Tag: LA.UM.7.5.r1-03100-8x96.0
    
    Change-Id: I2cfa7eeb03f19c9b6fb9be283146d0ef3f3dbe99

--- gps/Android.mk
+++ gps/Android.mk
@@ -1,5 +1,6 @@
 #
-# Copyright 2018 The LineageOS Project
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,5 +16,6 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+include $(LOCAL_PATH)/build/target_specific_features.mk
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
