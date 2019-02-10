49fd50a74f8db78961cf596a13c25a61bc540087
Bruno Martins <bgcngm@gmail.com>
2019-02-10 17:59:14 +0000

    msm8937-common: Build Touch HAL to enable KeyDisabler
    Change-Id: I1dbe4626cda4a90d77e92ac971679473564c2fd8

--- msm8937.mk
+++ msm8937.mk
@@ -359,6 +359,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(VENDOR_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
+# Touch HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.xiaomi_8937
+
 # Trust HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
